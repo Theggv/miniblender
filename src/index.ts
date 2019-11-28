ccb8053239d1c737d5eea659b5fd4de06cec22a0
ggv <m11rrrrr123@gmail.com>
2019-11-28 19:33:34 +0500

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,4 @@
-/*
-import {Renderer} from "./legacy/scene/Renderer";
-import {SceneControl} from "./legacy/scene/SceneControl";
-import {Scene} from "./legacy/scene/Scene";
-
-let canvas = new Renderer(
-    document.getElementById('canvas') as HTMLCanvasElement,
-    document.getElementById('canvas-buffer') as HTMLCanvasElement
-);
-
-let scene = new Scene(canvas);
-let control = new SceneControl(scene);
-
-*/
-
 import {Scene} from "./new/scene/Scene";
 
-let scene = new Scene(1280, 720);
+let scene: Scene;
+scene = new Scene(1280, 720);
